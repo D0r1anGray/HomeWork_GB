e42a79d4a8e3e8d0a16ee2dc6e2780062849479c
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-10 22:42:27 +0300

    Задача4. На вход натуральное число, на выходе цифры через запятую

---
+++ Introduction to programming languages/Seminar2/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+using System.ComponentModel.DataAnnotations;
+
+System.Console.WriteLine("Введите натуральное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 0){
+    System.Console.WriteLine("Вы ввели не натуральное число. Перезапустите программу");   
+}
+else{
+    System.Console.WriteLine("Цифры натурального числа через запятую, начиная с единиц");
+    while (n/10 != 0){
+        System.Console.Write(n%10 + ",");
+        n = n/10;
+    }
+    System.Console.Write(n%10); 
+}
