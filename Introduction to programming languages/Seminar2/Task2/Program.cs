767f7a1cfdb674e4dcfd6771f1771038699ad9c0
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-10 22:08:37 +0300

    Задача2. X и Y в какой четверти

---
+++ Introduction to programming languages/Seminar2/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y),
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+System.Console.WriteLine("Напишите координату точки X:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Напишите координату точки Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > 0 && y > 0){
+    System.Console.WriteLine("Точка в Первой четверти");
+}
+if (x < 0 && y > 0){
+    System.Console.WriteLine("Точка во Второй четверти");
+}
+if (x < 0 && y < 0){
+    System.Console.WriteLine("Точка в Третьей четверти");
+}
+if (x > 0 && y < 0){
+    System.Console.WriteLine("Точка в Четвёртой четверти");
+}
+else{
+    System.Console.WriteLine("X или Y равны 0. Перезапустите программу");
+}
