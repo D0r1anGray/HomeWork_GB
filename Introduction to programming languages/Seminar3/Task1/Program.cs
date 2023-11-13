df78c69b920340e236b43db475170e3b84b8038c
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-13 17:06:09 +0300

    Задание вывод количества элементов в пределах [20;90]

---
+++ Introduction to programming languages/Seminar3/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100.
+//Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+int[] mass = new int[10];
+int i = 0;
+
+System.Console.Write("Массив: ");  
+while (i < mass.Length){
+    mass[i] = new Random().Next(1,100);
+    System.Console.Write(mass[i] + " ");
+    i++;
+}
+System.Console.WriteLine();
+i = 0;
+int count = 0;
+while(i < mass.Length){
+    if(mass[i] >= 20 && mass[i] <= 90){
+        count ++;
+        i++;
+    }
+    else{
+        i++;
+    }
+}
+System.Console.WriteLine("В пределах отрезка [20;90] лежит " + count + " элементов массива");
