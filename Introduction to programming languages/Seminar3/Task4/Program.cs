aef582ed1b3b10335face402ee72629dba5f1e18
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-15 23:14:13 +0300

    Натуральный массив от 1 до 100 000. Ст.разряд на 0-ом индексе, Мл.разряд на последнем. Размер массива = кол-ву цифр

---
+++ Introduction to programming languages/Seminar3/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 4: Дано натуральное число в диапазоне от 1 до 100 000. Создайте массив, состоящий из цифр этого числа.
+//Старший разряд числа должен располагаться на 0-м индексе массива, младший – на последнем.
+//Размер массива должен быть равен количеству цифр.
+
+int N = new Random().Next(1,100000);
+
+String l = Convert.ToString(N);
+int[] massiv = new int[l.Length];
+
+System.Console.WriteLine("Натуральное число: " + N);
+//int L = l.Length;
+int i = 1;
+
+while(i < l.Length - 1){
+    massiv[l.Length - i] = N%10;
+    N = N/10;
+    i++; 
+}
+massiv[0] = N%10;
+
+i = 0;
+System.Console.Write("Массив: ");
+while(i < l.Length){
+    System.Console.Write(massiv[i] + " ");
+    i++;
+}
+System.Console.WriteLine("\nПервый элемент массива: " + massiv[0] + ". Последний элемент массива: " + massiv[l.Length - 1] +
+ ". Размер массива: " + massiv.Length);
