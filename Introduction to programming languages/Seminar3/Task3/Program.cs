fca11bba2bc5a2dcf98185f82b82a904202408bd
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-15 22:18:41 +0300

    Массив вещественных, найти разницу между макс и мин

---
+++ Introduction to programming languages/Seminar3/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью.
+//Найдите разницу между максимальным и минимальным элементов массива.
+double[] massiv = new double[5];
+
+int i = 0;
+System.Console.WriteLine("Введите поочерёдно вещественное значение каждого элемента массива:");
+while(i < massiv.Length){
+    System.Console.WriteLine("Введите значение " + (i+1) + " элемента массива: ");
+    massiv[i] = Convert.ToDouble(Console.ReadLine());
+    i++;
+}
+i = 0;
+
+double max = massiv[0];
+double min = massiv[0];
+System.Console.Write("Массив: ");
+while(i < massiv.Length){
+    if(massiv[i] > max){
+        max = massiv[i];
+    }
+    if(massiv[i] <= min){
+        min = massiv[i];
+    }
+    System.Console.Write(massiv[i] + " ");
+    i++;
+}
+System.Console.WriteLine("Максмум: " + max + " Минимум: " + min);
+System.Console.WriteLine("Разница между максимальным и минимальным значением массива: " + (max - min));
