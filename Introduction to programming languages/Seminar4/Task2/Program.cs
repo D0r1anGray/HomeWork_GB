0cf75121e21bf6f2dc4f2f8465331fb5d7cdd962
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-23 21:54:44 +0300

    Задача2. Массив со случайными 3-значными. Показывает кол-во четных

---
+++ Introduction to programming languages/Seminar4/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2: Задайте массив заполненный случайными трёхзначными числами.
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array = new int[15];
+int count = 0;
+
+void SeeArr(int[] arr){
+    System.Console.Write("Массив: ");
+    for(int i = 0; i < arr.Length; i++){
+        System.Console.Write(arr[i] + " ");
+    }
+}
+
+for(int i = 0; i < array.Length; i++){
+    array[i] = new Random().Next(100,999);
+    if(array[i]%2 == 0){
+        count++;
+    }
+}
+SeeArr(array);
+System.Console.WriteLine("\nКоличество четных чисел в массиве = " + count);
