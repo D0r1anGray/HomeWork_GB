3904aa6e644b744d2664c2cda9c7943eebe8e4aa
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-23 22:52:28 +0300

    3адача3. переворачивает массив

---
+++ Introduction to programming languages/Seminar4/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+int[] array =  new int[5];
+
+for(int i = 0; i < array.Length; i++){
+    array[i] = new Random().Next(1,100);
+}
+
+void SeeArr(int[] arr){
+    System.Console.Write("Массив: ");
+    for(int i = 0; i < arr.Length; i++){
+        System.Console.Write(arr[i] + " ");
+    }
+}
+
+int[] Reverse(int[] arr){
+    int temp;
+    for(int i = 0; i < arr.Length/2; i++){
+        temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+    return arr;
+}
+
+SeeArr(array);
+Reverse(array);
+System.Console.Write("\nПеревёрнутый ");
+SeeArr(array);
+
