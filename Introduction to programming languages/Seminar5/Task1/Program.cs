7fe36b9616b7c83e354d96bd0a0615a4e4889a90
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-28 22:04:28 +0300

    Задача1. Позиции элемента на вход, на выход его значение

---
+++ Introduction to programming languages/Seminar5/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+using System.Diagnostics;
+using System.Globalization;
+
+int[,] matrix = new int[10,10];
+
+for(int l = 0; l < matrix.GetLength(1); l++){
+    System.Console.Write(l + " ");
+}
+System.Console.WriteLine();
+for(int i = 0; i < matrix.GetLength(0); i++){
+    for(int j = 0; j < matrix.GetLength(1); j++){
+        matrix[i,j] = new Random().Next(1,10);
+        System.Console.Write(matrix[i,j] + " ");
+    }
+System.Console.WriteLine("|"+ i);
+}
+
+void Position(int x, int y){
+    if(x < matrix.GetLength(0) && y < matrix.GetLength(1)){
+        System.Console.WriteLine($"Элемент на позиции ({x};{y}) = {matrix[x,y]}");
+    }
+    else{
+        System.Console.WriteLine("Позиция вышла за край матрицы");
+    }
+}
+int x, y = 0;
+System.Console.Write("Введите координату Х элемента: ");
+x = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите координату Y элемента: ");
+y = Convert.ToInt32(Console.ReadLine());
+Position(x, y);
