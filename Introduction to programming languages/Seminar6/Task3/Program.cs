4dfbb2e40c31c678217f3041de19128947e4aad4
Dorian Gray <d0r1an.gray@yandex.ru>
2023-12-01 19:56:54 +0300

    Задача3. Строка, является ли палиндромом?

---
+++ Introduction to programming languages/Seminar6/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+System.Console.Write("Введите строку: ");
+string str = Console.ReadLine();
+
+void CheckPalindrome(string strok){
+    int count = 0;
+    for(int i = 0; i < strok.Length/2; i++){
+        if(strok[i] == strok[strok.Length-1 -i]){
+            count++;
+        }
+    }
+    if(count == strok.Length/2){
+        System.Console.WriteLine("Слово палиндром!");
+    }
+    else{
+        System.Console.WriteLine("Слово НЕ палиндром!");
+    }
+}
+
+CheckPalindrome(str);
