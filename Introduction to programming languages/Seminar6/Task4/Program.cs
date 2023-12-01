0ff006e626ac6ec909e2f3de8583771ed78df7fd
Dorian Gray <d0r1an.gray@yandex.ru>
2023-12-01 20:18:44 +0300

    Задача4. Строка из слов с пробелами. Получить строку с словами в обратном порядке и с пробелами

---
+++ Introduction to programming languages/Seminar6/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами.
+// Сформировать строку, в которой слова расположены в обратном порядке.
+// В полученной строке слова должны быть также разделены пробелами.
+
+System.Console.Write("Введите несколько слов через пробел: ");
+string[] strings = Console.ReadLine().Split(' ');
+
+string ReverseString(string[] str){
+    string strok = string.Empty;
+    for(int i = 0; i < str.Length; i++){
+        strok = str[i] +" "+ strok;
+    }
+    return strok;
+}
+
+System.Console.WriteLine(ReverseString(strings));
