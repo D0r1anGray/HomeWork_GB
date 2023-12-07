029cfc11ec969c80ef06218b9f650a0b0391a06b
Dorian Gray <d0r1an.gray@yandex.ru>
2023-12-07 19:38:24 +0300

    Задача1. Рекурсия на вывод натуральных чисел от M до N

---
+++ Introduction to programming languages/Seminar7/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+using System.Diagnostics;
+
+Console.Write("Введите число M как начало промежутка: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число N как конец промежутка: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void ReturnNatural(int beg, int end){
+    if(beg >= end || beg < 0 || end < 0){
+        return;
+    }
+    else{
+        Console.Write(beg + " ");
+        ReturnNatural(beg +1, end);
+    }
+}
+
+ReturnNatural(m, n);
+
+
+
+
