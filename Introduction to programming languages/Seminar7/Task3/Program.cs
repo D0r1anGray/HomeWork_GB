fc6f6642db37f94af990cf4d9773b0d51c89302c
Dorian Gray <d0r1an.gray@yandex.ru>
2023-12-07 19:57:55 +0300

    Задача3. Вывод элементов массива рекурсией

---
+++ Introduction to programming languages/Seminar7/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] mass = new int[10];
+
+int[] FillArray(int[] array){
+    for(int i = 0; i < array.Length; i++){
+        array[i] = new Random().Next(1,10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write(array[index] + " ");
+        PrintArray(array, index - 1);
+    }
+}
+
+FillArray(mass);
+PrintArray(mass, mass.Length-1);
